73b0f124ed2d87604df0e9ffc571461be7237dc4
daniel-j <daniel.j.hede@gmail.com>
2018-08-21 16:26:17 +0200

    Basic usdx notes txt parsing

--- src/app.cpp
+++ src/app.cpp
@@ -3,6 +3,7 @@
 #include <GL/glew.h>
 #include "app.hpp"
 #include <iostream>
+#include <fstream>
 // #include <portaudio.h>
 #include <soundio/soundio.h>
 #include "shader.hpp"
@@ -210,7 +211,7 @@ int App::init() {
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     // SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
-    mainWindow = SDL_CreateWindow("OpenGL",
+    mainWindow = SDL_CreateWindow("singsing",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         640, 480,
         SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
@@ -548,6 +549,76 @@ void ImageToTexture( GLuint tex, const char* imgpath ) {
 
 
 int App::launch() {
+
+    auto path = "../notes.txt";
+
+    std::ifstream filein(path);
+
+    auto finishedHeaders = false;
+    auto endFound = false;
+
+    for (std::string line; std::getline(filein, line); ) {
+        if (!line.empty() && *line.rbegin() == '\r') {
+            line.erase( line.length() - 1, 1);
+        }
+        auto len = line.length();
+        if (len == 0 || line[0] == ' ' || (finishedHeaders && line[0] == '#')) {
+            std::cerr << "Invalid linestart found in " << path << " :: \"" << line << "\". Aborting." << std::endl;
+            return 0;
+        }
+        if (!finishedHeaders && line[0] == '#') {
+            auto pos = line.find(':');
+            if (pos == std::string::npos) {
+                std::cerr << "Missing : in header \"" << line << "\"" << std::endl;
+                return 0;
+            }
+            auto identifier = line.substr(1, pos - 1);
+            auto value = line.substr(pos + 1);
+            if (value.empty()) {
+                std::cerr << "Empty value in header \"" << line << "\"" << std::endl;
+                return 0;
+            }
+
+            std::cout << identifier << " " << value << std::endl;
+        } else {
+            if (!finishedHeaders) {
+                finishedHeaders = true;
+            }
+
+            auto tag = line[0];
+            line = (len >= 2 && line[1] == ' ') ? line.substr(2) : line.substr(1);
+            // std::cout << tag << " " << line << std::endl;
+            int startBeat, length;
+
+            switch (tag) {
+                case 'E':
+                    endFound = true;
+                    break;
+                case 'P':
+                    // trimChars
+                    std::cout << "Got P: " << line << std::endl;
+                    break;
+                case ':':
+                case '*':
+                case 'F':
+                    auto pos1 = line.find(' ');
+                    startBeat = std::stoi(line.substr(0, pos1));
+                    auto pos2 = line.find(' ', pos1 + 1);
+                    length = std::stoi(line.substr(pos1 + 1, pos2 - pos1 - 1));
+                    pos1 = line.find(' ', pos2 + 1);
+                    int pitch = std::stoi(line.substr(pos2 + 1, pos1 - pos2 - 1));
+                    auto text = line.substr(pos1 + 1);
+                    if (text.empty()) {
+                        break;
+                    }
+                    printf("type: %c, startBeat: %d, length: %d, pitch: %d, text: \"%s\"\n", tag, startBeat, length, pitch, text.c_str());
+            }
+        }
+    }
+    if (!finishedHeaders) {
+        std::cerr << "Lyrics/Notes missing" << std::endl;
+        return 0;
+    }
 
     Shader square;
     square.init("../shaders/vert.glsl", "../shaders/frag.glsl");
