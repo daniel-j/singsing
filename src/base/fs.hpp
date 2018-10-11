2e0367bf97bedfd9237561c3a0c4729abcf5cd34
daniel-j <daniel.j.hede@gmail.com>
2018-10-11 16:19:06 +0200

    add fs header

---
+++ src/base/fs.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifdef WINDOWS
+#include <filesystem>
+namespace fs = std::filesystem;
+#else
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#endif
